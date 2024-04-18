ad925f2474afd64ce4efdd62bd86b9a764413839
bahri444 <bahrysaipul266@gmail.com>
2024-04-18 07:53:39 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $percobaan = User::where('jenis_anggota', 'percobaan')->get()->count();
+        $pasti = User::where('jenis_anggota', 'pasti')->get()->count();
+        $kehormatan = User::where('jenis_anggota', 'kehormatan')->get()->count();
         return view('superadmin.dashboard', [
             'title' => 'dashboard',
-            'data' => 'mydata'
+            'percobaan' => $percobaan,
+            'pasti' => $pasti,
+            'kehormatan' => $kehormatan,
         ]);
     }
 }
